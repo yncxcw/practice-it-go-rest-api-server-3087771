56c21c135c2dd5efc23756d5d921a5c122d5f972
yncxcw <ynjassionchen@gmail.com>
2023-08-07 02:21:05 +0000

    db test

--- src/practiceit/go.mod
+++ src/practiceit/go.mod
@@ -4,4 +4,4 @@ go 1.18
 
 replace example.com/backend => ../backend
 
-require example.com/backend v0.0.0-00010101000000-000000000000
+require github.com/mattn/go-sqlite3 v1.14.17
--- src/practiceit/practiceit.go
+++ src/practiceit/practiceit.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"example.com/backend"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Product struct {
+	Id        int
+	Name      string
+	Inventory int
+	Price     int
+}
+
 func main() {
-	backend.Run(":9003")
+	// backend.Run(":9003")
+	fmt.Println("Opening table")
+	db, err := sql.Open("sqlite3", "../../practiceit.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Querying tables")
+	rows, err := db.Query("SELECT id, name, inventory, price FROM products")
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for rows.Next() {
+		var p Product
+		rows.Scan(&p.Id, &p.Name, &p.Inventory, &p.Price)
+		println(p.Id, p.Name, p.Inventory, p.Price)
+	}
 }
