1d40dbbf5dbd4b12bb566d42a325be820510b292
yncxcw <ynjassionchen@gmail.com>
2023-08-07 02:58:57 +0000

    local

--- src/backend/backend.go
+++ src/backend/backend.go
@@ -1,17 +1,51 @@
 package backend
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Product struct {
+	Id        int
+	Name      string
+	Inventory int
+	Price     int
+}
+
+type App struct {
+	DB   *sql.DB
+	Port string
+}
+
+func (app *App) Init() {
+	app.DB, _ = sql.Open("sqlite3", "../../practiceit.db")
+}
+
+func (p *Product) Scan(db *sql.DB) {
+	rows, err := db.Query("SELECT id, name, inventory, price FROM products")
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for rows.Next() {
+		var p Product
+		rows.Scan(&p.Id, &p.Name, &p.Inventory, &p.Price)
+		println(p.Id, p.Name, p.Inventory, p.Price)
+	}
+
+}
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World\n")
 }
 
-func Run(addr string) {
+func (app *App) Run() {
 	http.HandleFunc("/", helloWorld)
-	fmt.Println("Server started and listening on port ", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	fmt.Println("Server started and listening on port ", app.Port)
+	log.Fatal(http.ListenAndServe(app.Port, nil))
 }
--- src/practiceit/practiceit.go
+++ src/practiceit/practiceit.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"example.com/backend"
 )
 
-type Product struct {
-	Id        int
-	Name      string
-	Inventory int
-	Price     int
-}
-
 func main() {
-	// backend.Run(":9003")
-	fmt.Println("Opening table")
-	db, err := sql.Open("sqlite3", "../../practiceit.db")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println("Querying tables")
-	rows, err := db.Query("SELECT id, name, inventory, price FROM products")
-	defer rows.Close()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	for rows.Next() {
-		var p Product
-		rows.Scan(&p.Id, &p.Name, &p.Inventory, &p.Price)
-		println(p.Id, p.Name, p.Inventory, p.Price)
-	}
+	app := &backend.App{}
+	app.Port = ":9001"
+	app.Init()
+	app.Run()
 }
